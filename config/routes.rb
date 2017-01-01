e7ff532f0af66fb472ad27023da6485fe3369a1c
Liu Yuan Xin <yuanxin@YuandeiMac.local>
2017-01-02 07:40:43 +0800

    join & quit group action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :groups do
+    #add groups and quit groups =
+    member do
+      post :join
+      post :quit
+    end
+    
     resources :posts
   end
   root 'groups#index'
