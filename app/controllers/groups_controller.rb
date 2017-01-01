3909a17ff3e03dbe886df2188ddb55dc0d22841b
Liu Yuan Xin <yuanxin@YuandeiMac.local>
2017-01-01 11:49:11 +0800

    implement groups#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,8 +2,12 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
-  
+
   def show
+    @group = Group.find(params[:id])
+  end
+
+  def edit
     @group = Group.find(params[:id])
   end
 
@@ -16,6 +20,13 @@ class GroupsController < ApplicationController
 
     redirect_to groups_path
   end
+  
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to group_path, notice: "update Success"
+  end
+
   private
 
   def group_params
